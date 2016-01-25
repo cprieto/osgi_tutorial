e574a3be8e4c85772a2feefa457d5b0612d5b194
Cristian Prieto <me@cprieto.com>
2016-01-25 17:24:11 +1100

    We have the simplest activator with some stuff

--- src/main/kotlin/example1.kt
+++ src/main/kotlin/example1.kt
@@ -1,4 +1,25 @@
 package tutorial.example1
 
-public class KotlinActivator {
+import org.osgi.framework.BundleActivator
+import org.osgi.framework.BundleContext
+import org.osgi.framework.ServiceEvent
+import org.osgi.framework.ServiceListener
+
+class KotlinActivator: BundleActivator, ServiceListener {
+    override fun serviceChanged(event: ServiceEvent?) {
+        val svc_class = event?.serviceReference?.getProperty("objectClass")
+        when (event?.type) {
+            ServiceEvent.REGISTERED -> println("Service of type $svc_class registered.")
+            ServiceEvent.UNREGISTERING -> println("Service of type $svc_class unregistered.")
+            ServiceEvent.MODIFIED -> println("Service of type $svc_class modified.")
+        }
+    }
+
+    override fun start(context: BundleContext?) {
+        println("Hi from Kotlin (in OSGi)!")
+    }
+
+    override fun stop(context: BundleContext?) {
+        println("Oh kntx.")
+    }
 }
